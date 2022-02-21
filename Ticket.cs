6d5133f76a1b080d28c3ffa736c44c88962aa59d
Joseph Radi <jradi2@my.wctc.edu>
2022-02-21 14:59:22 -0600

    Began reading from ticket file & changes to Ticket

--- Ticket.cs
+++ Ticket.cs
@@ -12,10 +12,32 @@ namespace TicketingSystem
         }
         public int ticketID { get; set; }
         public string summary { get; set; }
-        public bool status { get; set; }
-        public Level priority { get; set; }
+        public bool status;
+        public Level priority;
         public string submitter { get; set; }
         public string assigned { get; set; }
         public List<string> watching { get; set; }
+
+        public Ticket(int ticketID, string summary, bool status, Level priority, string submitter, string assigned, List<string> watching) {
+            this.ticketID = ticketID;
+            this.summary = summary;
+            this.status = status;
+            this.priority = priority;
+            this.submitter = submitter;
+            this.assigned = assigned;
+            this.watching = watching;
+        }
+
+        public Ticket() {}
+
+        public string getStatus() {
+            return status? "Open" : "Closed";
+        }
+
+        public void setStatus(string status) {
+            this.status = status.ToLower() == "open" ? true : false;
+        }
+
+        // TODO: set & get priority
     }
 }
--- TicketFile.cs
+++ TicketFile.cs
@@ -1,5 +1,9 @@
 using System;
 using System.Collections.Generic;
+using NLog.Web;
+using System.IO;
+using System.Linq;
+
 
 namespace TicketingSystem
 {
@@ -7,9 +11,26 @@ namespace TicketingSystem
     public class TicketFile {
         public string filePath { get; set; }
         public List<Ticket> tickets;
+        private static NLog.Logger logger = NLogBuilder.ConfigureNLog(Directory.GetCurrentDirectory() + "\\nlog.config").GetCurrentClassLogger();
 
         public TicketFile(string filePath) {
             this.filePath = filePath;
+
+            tickets = new List<Ticket>();
+            StreamReader sr = new StreamReader(filePath);
+            sr.ReadLine();
+            while (!sr.EndOfStream) {
+                string[] line = sr.ReadLine().Split(',');
+                Ticket ticket = new Ticket();
+
+                ticket.ticketID = Int32.Parse(line[0]);
+                ticket.summary = line[1];
+                ticket.status = line[2] == "Open" ? true : false;
+                ticket.submitter = line[4];
+                ticket.assigned = line[5];
+
+                // TODO: set priority & watching
+            }
         }
     }
 }
